cb540d836d60b280cb15e61ed45c10a04d3b4b97
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-12-20 01:39:06 +0300

    Added pagination

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllmeetPageComponent } from './pages/allmeet-page/allmeet-page.component';
+import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { HowtoPageComponent } from './pages/howto-page/howto-page.component';
+import { MymeetPageComponent } from './pages/mymeet-page/mymeet-page.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AuthPageComponent },
+  { path: 'howto', component: HowtoPageComponent },
+  { path: 'all-meetups', component: AllmeetPageComponent, canActivate: [AuthGuard] },
+  { path: 'my-meetups', component: MymeetPageComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './components/header/header.module';
+import { AuthPageModule } from './pages/auth-page/auth-page.module';
+import { HowtoPageModule } from './pages/howto-page/howto-page.module';
+import { AllmeetPageModule } from './pages/allmeet-page/allmeet-page.module';
+import { JwtInterceptor } from './shared/interceptors/jwt.interceptor';
+import { MymeetPageModule } from './pages/mymeet-page/mymeet-page.module';
+import { FilterMinePipe } from './shared/pipes/filter-mine/filter-mine.pipe';
+import { FilterMeetupsPipe } from './shared/pipes/filter-meetups/filter-meetups.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeaderModule
+    HttpClientModule,
+    HeaderModule,
+    AuthPageModule,
+    HowtoPageModule,
+    AllmeetPageModule,
+    MymeetPageModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
