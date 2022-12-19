41816712d087017ba189c41be62b3ee49d653aaf
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-12-20 01:39:48 +0300

    Created app header [WIP]

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  authStatus!: boolean;
+  adminStatus!: boolean;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.authService.isAuthed.subscribe(value => {
+      this.authStatus = value;
+    });
+
+    this.authService.isAdmin.subscribe(value => {
+      this.adminStatus = value;
+    })
+  }
+
+  logoutHandler() {
+    this.authService.logout();
+  }
+
+  signInHandler() {
+    this.router.navigate(['']);
+  }
+
+  settingsHandler() {
+    console.log("Not done yet!");
+  }
+
+  travelHandler(route: string) {
+    this.router.navigate([route]);
+  }
 }
