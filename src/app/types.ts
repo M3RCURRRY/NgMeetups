0a97608e6cec2008fdbb055c22873b9a3a1f55f1
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-12-20 01:42:10 +0300

    Added types for http requests

---
+++ src/app/types.ts
@@ -0,0 +1,48 @@
+interface IUserData {
+  id: number;
+  email: string;
+  exp: number;
+  iat: number;
+  roles: IRole[]
+}
+
+interface IRole {
+  id: number;
+  createdAt: string;
+  name: string;
+  updatedAt: string;
+  UserRole: IUserRole;
+}
+
+interface IUserRole {
+  id: number;
+  userId: number;
+  roleId: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface IMeetupData {
+  id: number;
+  name: string;
+  description: string;
+  location: string;
+  target_audience: string;
+  need_to_know: string;
+  will_happen: string;
+  reason_to_come: string;
+  time: string;
+  duration: number;
+  createdBy: number;
+  owner: IUserDesc;
+  users: IUserDesc[];
+}
+
+interface IUserDesc {
+  id: number;
+  email: string;
+  password: string;
+  fio: string;
+}
+
+export { IUserData, IRole, IUserRole, IMeetupData, IUserDesc }
