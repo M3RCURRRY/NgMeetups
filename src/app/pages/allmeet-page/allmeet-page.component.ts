b95be0e925b0ab43f00f69160e7968146e79204c
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-12-22 22:53:57 +0300

    Added filtering reactive input

--- src/app/pages/allmeet-page/allmeet-page.component.ts
+++ src/app/pages/allmeet-page/allmeet-page.component.ts
@@ -1,21 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MeetupsService } from 'src/app/services/meetups.service';
 import { IMeetupData } from 'src/app/types';
 
 @Component({
   selector: 'app-allmeet-page',
   templateUrl: './allmeet-page.component.html',
-  styleUrls: ['./allmeet-page.component.scss']
+  styleUrls: ['./allmeet-page.component.scss'],
 })
 export class AllmeetPageComponent implements OnInit {
-
   allMeetups!: IMeetupData[];
+  
+  filterForm!: FormGroup<{
+    searchValue: FormControl<string | null>
+  }>;
+  filter!: string;
 
-  constructor(private meetService: MeetupsService) { }
+  constructor(
+    private meetService: MeetupsService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
+
+  createMeetupHandler() {
+    this.router.navigate(['create-meetup']);
+  }
 
   ngOnInit(): void {
-    this.meetService.meetups.subscribe(value => {
+    this.meetService.meetups.subscribe((value) => {
       this.allMeetups = value;
+    });
+
+    this.initForm();
+    this.filterForm.valueChanges.subscribe((data) => {
+      this.filter = data.searchValue ? data.searchValue : ''
+    })
+  }
+
+  initForm() {
+    this.filterForm = this.fb.group({
+      searchValue: ['']
     })
   }
 }
--- src/app/pages/allmeet-page/allmeet-page.module.ts
+++ src/app/pages/allmeet-page/allmeet-page.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllmeetPageComponent } from './allmeet-page.component';
 import { MeetupItemModule } from 'src/app/components/meetup-item/meetup-item.module';
+import { ButtonModule } from 'src/app/components/button/button.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FilterMeetupsModule } from 'src/app/shared/pipes/filter-meetups/filter-meetups.module';
 
 
 
@@ -11,7 +14,11 @@ import { MeetupItemModule } from 'src/app/components/meetup-item/meetup-item.mod
   ],
   imports: [
     CommonModule,
-    MeetupItemModule
+    MeetupItemModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FilterMeetupsModule
   ]
 })
 export class AllmeetPageModule { }
